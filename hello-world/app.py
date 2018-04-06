a587cf92e777abe79e0ba61c0256ddb9ce1cd88d
horeilly <hor13492@gmail.com>
2018-04-05 23:52:20 -0700

    bond route added

--- hello-world/app.py
+++ hello-world/app.py
@@ -1,19 +1,29 @@
-from flask import Flask
-
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/")
-def index():
-    return "<h1>Hello, World!</h1>"
+bonds = {}
 
 
-@app.route("/<name>")
-def greet(name):
-    return "<h1>Hello, {}</h1>".format(name)
+class Bond(Resource):
+
+    @staticmethod
+    def get(bond_name):
+        return {bond_name: bonds[bond_name]}
+
+    @staticmethod
+    def post(bond_name):
+        bonds[bond_name] = request.json
+        bonds[bond_name]["coupon_payments"] = bonds[bond_name]["principal"] * bonds[bond_name]["coupon"]
+        return {bond_name: bonds[bond_name]}
+
+
+api.add_resource(Bond, "/new/bond/<string:bond_name>")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=True)
 
